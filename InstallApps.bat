ad6e202a3f5eeccb8fd121d316534c5ea241f127
juliendvs <juliandeves@outlook.fr>
2024-05-11 07:57:23 +0200

    First commit files install bat

---
+++ InstallApps.bat
@@ -0,0 +1,53 @@
+@echo off
+
+:: install 7zip
+choco install 7zip -y
+
+:: Install Mozilla Thunderbird
+choco install thunderbird -y
+
+:: Install Audacity
+choco install audacity -y
+
+:: Install Adobe Acrobat Reader
+choco install adobereader -y
+
+:: Install Bitwarden
+choco install bitwarden -y
+
+:: Install CrystalDiskInfo
+choco install crystaldiskinfo -y
+
+:: Install CrystalDiskMark
+choco install crystaldiskmark -y
+
+:: Install Ferdium
+choco install ferdi -y
+
+:: Install Hyper
+choco install hyper -y
+
+:: Install MSI Afterburner
+choco install msiafterburner -y
+
+:: Install OBS Studio
+choco install obs-studio -y
+
+:: Install Obsidian
+choco install obsidian -y
+
+:: Install Opera GX
+choco install operagx -y
+
+:: Install PowerToys
+choco install powertoys -y
+
+:: Install Sublime Text
+choco install sublimetext -y
+
+:: Install VLC Media player
+choco install vlc -y
+
+echo All applications installed successfully.
+
+pause
